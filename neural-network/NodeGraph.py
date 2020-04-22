09837edf1db62d6b5c07755b640386a4e5014033
Chengyuan Yan <yanchengyuan@gmail.com>
2020-04-21 23:21:27 -0700

    Updated NodeGraph.py

--- neural-network/NodeGraph.py
+++ neural-network/NodeGraph.py
@@ -1,7 +1,12 @@
+import json
 import numpy as np
+import time
+
 import ActivationFunction
 import CostFunction
-import json
+import ModelTrainerBase
+from MachineLearningUtils import Utils
+
 
 class Node:
     """
@@ -86,7 +91,7 @@ class Node:
         inputCount = len(self.mInputs)
         # TODO: compare with He initialization
         weightStdDev = np.sqrt(1.0 / inputCount)
-        self.mInputWeights = np.random.normal(0.0, weightStdDev, (inputCount, 1))
+        self.mInputWeights = np.random.normal(0.0, weightStdDev, inputCount)
         self.mInputBias = np.random.normal(0.0, 1.0)
 
         self.mDeltaInputWeights = np.zeros(inputCount)
@@ -98,6 +103,9 @@ class Node:
         :return activation of the neuron
         """
         self.mLatestInputs = np.array([n.getLatestActivation() for n in self.mInputs])
+        #print("mLatestInputs = ", self.mLatestInputs)
+        #print("mInputWeights = ", self.mInputWeights)
+        #print("mInputBias = ", self.mInputBias)
         z = np.dot(self.mLatestInputs, self.mInputWeights) + self.mInputBias
         self.mLatestActivation = self.mActivationFunction.getValue(z)
         self.mLatestActivationDerivative = self.mActivationFunction.getDerivative(z)
@@ -114,7 +122,7 @@ class Node:
         # par_C_over_par_z = par_C_over_par_a * d_a_over_d_z
         self.mLatestCostPartialDerivative = par_C_over_par_a * self.mLatestActivationDerivative
 
-        self.mDeltaInputWeights += self.mLatestCostPartialDerivative * self.mLatestInput
+        self.mDeltaInputWeights += self.mLatestCostPartialDerivative * self.mLatestInputs
         self.mDeltaInputBias += self.mLatestCostPartialDerivative
         self.mSampleCount += 1
         return self.mLatestCostPartialDerivative
@@ -124,7 +132,10 @@ class Node:
         """
         update parameters using gradient descent
         """
-        k = learningRate / mSampleCount
+        if (0 == self.mSampleCount):
+            return
+
+        k = learningRate / self.mSampleCount
         self.mInputWeights -= k * self.mDeltaInputWeights
         self.mInputBias -= k * self.mDeltaInputBias
 
@@ -147,24 +158,24 @@ class InputNode(Node):
     def initialize(self):
         pass
 
-    def feedforward(self):
+    def feedforward(self, x):
         self.mLatestActivation = x
         return self.mLatestActivation
 
     def backPropagate(self):
-        raise RuntimeError("An InputNode does not backPropagate")
+        pass
 
     def update(self, learningRate):
-        raise RuntimeError("An InputNode does not update")
+        pass
 
 class OutputNode(Node):
     def __init__(self, id, activationFunction):
-        Node.__init__(self, id, ActivationFunction, "output")
+        Node.__init__(self, id, activationFunction, "output")
 
     def backPropagate(self, costPartialDerivative):
         self.mLatestCostPartialDerivative = costPartialDerivative
 
-        self.mDeltaInputWeights += self.mLatestCostPartialDerivative * self.mLatestInput
+        self.mDeltaInputWeights += self.mLatestCostPartialDerivative * self.mLatestInputs
         self.mDeltaInputBias += self.mLatestCostPartialDerivative
         self.mSampleCount += 1
         return self.mLatestCostPartialDerivative
@@ -172,20 +183,28 @@ class OutputNode(Node):
 class Graph:
     def __init__(self):
         # only indices
-        self.mInputNodeIndices = []
-        self.mOutputNodeIndices = []
-        # all node instances, sorted topologically
-        self.mNodes = []
+        self.mInputNodes = []
+        self.mOutputNodes = []
+        # all hidden nodes sorted topologically
+        self.mHiddenNodes = []
         self.mCostFunction = None
 
         self.mBatchSize = 1
         self.mLearningRate = 0.1
+        self.mMaxEpoch = 30
 
     def saveToFile(self, filename):
-        pass
+        print(f"saving to file '{filename}'")
+        with open(filename, "wb") as f:
+            pickle.dump(self.__dict__, f)
+        print("done")
 
     def loadFromFile(self, filename):
-        pass
+        print(f"loading from file '{filename}'")
+        with open(filename, "rb") as f:
+            d = pickle.load(f)
+            self.__dict__.update(d)
+        print("done")
 
     def initialize(self, jsonStr):
         """
@@ -199,6 +218,7 @@ class Graph:
             "hyperparameters": {
                 "learning_rate": float
                 "batch_size": int
+                "max_epoch": int
             }
         }
 
@@ -234,10 +254,10 @@ class Graph:
             tag = n["tag"]
             if ("input" == tag):
                 nodes.update({id: InputNode(id)})
-                self.mInputNodeIndices.append(id)
+                self.mInputNodes.append(nodes[id])
             elif ("output" == tag):
                 nodes.update({id: OutputNode(id, ActivationFunction.create(af))})
-                self.mOutputNodeIndices.append(id)
+                self.mOutputNodes.append(nodes[id])
             elif ("hidden" == tag):
                 nodes.update({id: Node(id, ActivationFunction.create(af), tag)})
             else:
@@ -259,13 +279,21 @@ class Graph:
         # parse hyperparameters
         self.mLearningRate = float(hyperparameters["learning_rate"])
         self.mBatchSize = int(hyperparameters["batch_size"])
+        self.mMaxEpoch = int(hyperparameters["max_epoch"])
 
         # topological sort
         nodeCount = len(nodes)
+        hiddenNodeCount = nodeCount - len(self.mInputNodes) - len(self.mOutputNodes)
         processed = [False for i in range(nodeCount)]
-        self.mNodes.clear()
 
-        for i in range(nodeCount):
+        for n in self.mInputNodes:
+            id = n.id
+            processed[id] = True
+            for j in range(nodeCount):
+                indegrees[j].discard(id)
+
+        self.mHiddenNodes.clear()
+        for i in range(hiddenNodeCount):
             id = None
             for j in range(nodeCount):
                 if ((not processed[j]) and (len(indegrees[j]) == 0)):
@@ -275,30 +303,127 @@ class Graph:
                 raise RuntimeError("Not a valid graph")
 
             processed[id] = True
-            self.mNodes.append(nodes[id])
+            self.mHiddenNodes.append(nodes[id])
             for j in range(nodeCount):
                 indegrees[j].discard(id)
 
+        # initialize nodes
+        for n in self.mInputNodes:
+            n.initialize()
+        for n in self.mHiddenNodes:
+            n.initialize()
+        for n in self.mOutputNodes:
+            n.initialize()
+
     def feedforward(self, x):
         """
-        :return yHat
+        :param [in] x: input
+        :return yHat: prediction
         """
-        pass
+        for (i, n) in enumerate(self.mInputNodes):
+            n.feedforward(x[i])
 
-    def train(self, x, y):
-        pass
+        for n in self.mHiddenNodes:
+            n.feedforward()
+
+        for n in self.mOutputNodes:
+            n.feedforward()
+
+        yHat = np.array([n.getLatestActivation() for n in self.mOutputNodes])
+        return yHat
+
+
+    def backPropagate(self, dC_over_dyHat):
+        for (i, n) in enumerate(self.mOutputNodes):
+            n.backPropagate(dC_over_dyHat[i])
+
+        for n in reversed(self.mHiddenNodes):
+            n.backPropagate()
+
+
+    def update(self):
+        for n in self.mOutputNodes:
+            n.update(self.mLearningRate)
+
+        for n in self.mHiddenNodes:
+            n.update(self.mLearningRate)
+
+
+    def train(self, xs, ys):
+        """
+        :param [in] xs: input; each row represents an input
+        :param [in] ys: ground truth output; each row represents an output
+        """
+        print("training")
+        indices = np.arange(xs.shape[0])
+        for epoch in range(self.mMaxEpoch):
+            startTime = time.time()
+
+            np.random.shuffle(indices)
+            for i in range(xs.shape[0]):
+
+                yHat = self.feedforward(xs[indices[i], :])
+
+                dC_over_dyHat = self.mCostFunction.getDerivative(ys[indices[i], :], yHat)
+
+                self.backPropagate(dC_over_dyHat)
+
+                if ((1 + i) % self.mBatchSize == 0):
+                    self.update()
+                    #labels = np.zeros(4)
+                    #labels[np.argmax(yHat)] = 1
+                    #print(xs[indices[i], :], " --> ", ys[indices[i], :], " --> ", labels)
+            self.update()
+
+            endTime = time.time()
+
+            accuracy = self.validate(xs, ys)
+
+            print(f"==> epoch %d: time = %.2fs, accuracy = %.2f%%" %
+                    (epoch, endTime - startTime, accuracy * 100))
+
+    def validate(self, xs, ys):
+        """
+        :return: accuracy
+        """
+        yHats = np.array([self.feedforward(x) for x in xs])
+        labels = Utils.getPredictions(yHats)
+        #for (y, l) in zip(ys, labels):
+        #    print(y, " --> ", l)
+        accuracy = Utils.compare(labels, ys)
+
+        return accuracy
+
 
     def print(self):
-        print(f"[graph] %d nodes (%d input, %d output), %s cost function" % (
-            len(self.mNodes),
-            len(self.mInputNodeIndices),
-            len(self.mOutputNodeIndices),
+        print(f"[graph] %d input nodes, %d hidden nodes, %d output nodes, %s cost function" % (
+            len(self.mInputNodes),
+            len(self.mHiddenNodes),
+            len(self.mOutputNodes),
             self.mCostFunction.getName()))
 
-        for n in self.mNodes:
+        for n in self.mInputNodes:
+            n.print()
+        for n in self.mHiddenNodes:
+            n.print()
+        for n in self.mOutputNodes:
             n.print()
 
-    def doTopologicalSort():
+
+class Trainer(ModelTrainerBase.ModelTrainerBase):
+    def __init__(self, dataset, config):
+        self.mGraph = Graph()
+        ModelTrainerBase.__init__(self)
+
+    def parseConfig(self, config):
+        with open(config, "r") as f:
+            jsonStr = f.read()
+            self.mGraph.initialize(jsonStr)
+
+    def trainModel(self):
+        pass
+
+    def getResult(self):
         pass
 
 
@@ -310,3 +435,14 @@ if (__name__ == "__main__"):
 
     g.print()
 
+    # input: 4 values between 0 and 1
+    # output: which pixel is max
+    count = 10000
+    xs = np.random.random((count, 4))
+    ys = Utils.getPredictions(xs)
+
+    #print("input and output")
+    #for (x, y) in zip(xs, ys):
+    #    print(x, " --> ", y)
+
+    g.train(xs, ys)
